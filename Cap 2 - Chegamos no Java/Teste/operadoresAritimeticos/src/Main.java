40c04dea9e585168893ac51bd8a5f577abd44b88
BiahRosa <beatriz.rosa.2005@gmail.com>
2025-02-18 15:49:57 -0300

    Initial commit

---
+++ Cap 2 - Chegamos no Java/Teste/operadoresAritimeticos/src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+                //Operadores Aritimeticos
+                // + Soma
+                // - Subtração
+                // * Multiplição
+                // / Divisão
+                // % Prcentagem
+                // ++ Incremento
+                // -- Decremento
+                int x = 10;
+                int y = 5;
+                int soma = x + y;
+                System.out.println(soma);
+                int multiplicacao = x * 2;
+                System.out.println(multiplicacao);
+                float f = 5;
+                System.out.println(f / 2);
+                int resto = y % 2;
+                System.out.println(resto);
+                x++;
+                System.out.println(x);
+                y--;
+                System.out.println(y);
+
+
+    }
+}
---
+++ Cap 2 - Chegamos no Java/Teste2.0/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Tipos primitivos
+        //byte, short, int, long -- números inteiros
+        int idade;
+        idade = 22;
+        //float, double -- informação fracionária
+        float altura = 1.80f;
+        //char -- único caracter ALFANUMÉRICO
+        char genero = 'f';
+        //boolean -- váriavel lógica (verdadeiro ou falso)
+        boolean especial = false;
+
+        //Tipo de referência
+        //String -- onde vamos armazenar o conjuto de caracteres, texto
+        String nome = "Thiago Yamamoto";
+    }
+}
